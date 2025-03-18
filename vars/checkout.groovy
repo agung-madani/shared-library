f0f230c10bfcefd54dabd6ebd7bfa37b5ac7b270
20106050083 <agungrashif009@gmail.com>
2025-03-18 09:52:33 +0700

    checkout 2

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -2,11 +2,32 @@ def call(Map config = [:]) {
     pipeline {
         agent any
 
+        parameters {
+            string(name: "git_repo", defaultValue: config.get("git_repo", ""), trim: true, description: "Git repository URL")
+            gitParameter(
+                name: "git_branch",
+                defaultValue: "",
+                type: "PT_TAG",
+                branch: "master",
+                branchFilter: ".*",
+                tagFilter: "*",
+                sortMode: "DESCENDING_SMART",
+                selectedValue: "TOP",
+                quickFilterEnabled: true,
+                useRepository: config.get("git_repo", ""),
+                listSize: 5
+            )
+        }
+
         stages {
             stage('Checkout') {
                 steps {
                     script {
-                        checkout([$class: 'GitSCM', branches: [[name: "${params.git_branch}"]], userRemoteConfigs: [[url: "${params.git_repo}", credentialsId: 'agung-github']]])
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: params.git_branch]],
+                            userRemoteConfigs: [[url: params.git_repo, credentialsId: 'agung-github']]
+                        ])
                     }
                 }
             }
