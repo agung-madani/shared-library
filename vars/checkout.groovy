15054532ce2ee91387d27394f50b9316045508e1
20106050083 <agungrashif009@gmail.com>
2025-03-18 10:03:58 +0700

    checkout 3

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,36 +1,14 @@
+// vars/checkout.groovy - Shared Library Function
 def call(Map config = [:]) {
-    pipeline {
-        agent any
+    // Ensure parameters are provided correctly
+    def gitRepo = config.get('git_repo', '')
 
-        parameters {
-            string(name: "git_repo", defaultValue: config.get("git_repo", ""), trim: true, description: "Git repository URL")
-            gitParameter(
-                name: "git_branch",
-                defaultValue: "",
-                type: "PT_TAG",
-                branch: "master",
-                branchFilter: ".*",
-                tagFilter: "*",
-                sortMode: "DESCENDING_SMART",
-                selectedValue: "TOP",
-                quickFilterEnabled: true,
-                useRepository: config.get("git_repo", ""),
-                listSize: 5
-            )
-        }
-
-        stages {
-            stage('Checkout') {
-                steps {
-                    script {
-                        checkout([
-                            $class: 'GitSCM',
-                            branches: [[name: params.git_branch]],
-                            userRemoteConfigs: [[url: params.git_repo, credentialsId: 'agung-github']]
-                        ])
-                    }
-                }
-            }
-        }
+    // Run checkout step
+    script {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/main']],  // Use "main" as default if branch is not specified
+            userRemoteConfigs: [[url: gitRepo, credentialsId: 'agung-github']]
+        ])
     }
 }
