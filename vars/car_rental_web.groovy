3dc4c8341797956ffd4c629920f80e2d02fcea66
20106050083 <agungrashif009@gmail.com>
2025-03-24 11:23:25 +0700

    debug 14

--- vars/car_rental_web.groovy
+++ vars/car_rental_web.groovy
@@ -9,24 +9,24 @@ def call(Map config = [:]) {
         parameters {
             string(name: "git_repo", defaultValue: "${config.git_repo}", trim: true, description: "Git repository URL")
             
-            // gitParameter(
-            //     name: 'TAG',
-            //     description: 'Select the tag to build',
-            //     type: 'PT_TAG',                 
-            //     defaultValue: '',               
-            //     branch: 'origin/main',
-            //     tagFilter: '*',                 
-            //     sortMode: 'DESCENDING_SMART',   
-            //     selectedValue: 'NONE',          
-            //     useRepository: "${config.git_repo}"
-            // )
+            gitParameter(
+                name: 'TAG',
+                description: 'Select the tag to build',
+                type: 'PT_TAG',                 
+                defaultValue: '',               
+                branch: 'origin/main',
+                tagFilter: '*',                 
+                sortMode: 'DESCENDING_SMART',   
+                selectedValue: 'NONE',          
+                useRepository: "${config.git_repo}"
+            )
         }
         
         stages {
             stage('Checkout') {
                 steps {
                     checkout scmGit(
-                        // branches: [[name: "refs/tags/${params.TAG}"]], 
+                        branches: [[name: "refs/tags/${params.TAG}"]], 
                         extensions: [], 
                         userRemoteConfigs: [[
                             credentialsId: '81f0e0bd-57fe-41ed-9443-ffff09c3fcc0', 
@@ -34,19 +34,19 @@ def call(Map config = [:]) {
                         ]]
                     )
                     
-                    // echo "Building from tag: ${params.TAG}"
+                    echo "Building from tag: ${params.TAG}"
                 }
             }
             
         }
         
-        // post {
-        //     success {
-        //         echo "Successfully built and processed tag: ${params.TAG}"
-        //     }
-        //     failure {
-        //         echo "Failed to build tag: ${params.TAG}"
-        //     }
-        // }
+        post {
+            success {
+                echo "Successfully built and processed tag: ${params.TAG}"
+            }
+            failure {
+                echo "Failed to build tag: ${params.TAG}"
+            }
+        }
     }
 }
