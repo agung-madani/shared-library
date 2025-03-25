7c67a75b1dd00bfa5823a918ad5e7b6e63080fb7
20106050083 <agungrashif009@gmail.com>
2025-03-25 13:11:34 +0700

    new groovy

--- vars/car_rental_web.groovy
+++ vars/car_rental_web.groovy
@@ -10,8 +10,13 @@ def call(Map config = [:]) {
             OCP_PROJECT = "armada009-dev"
             OCP_CLUSTER_URL = "https://api.rm2.thpm.p1.openshiftapps.com:6443"
             OCP_TOKEN = "sha256~y7E_DwDmzSlhc-Ss4CfTZdmcX9T2CX3bJi8wVF1QAKo"
+            OCP_REGISTRY = "default-route-openshift-image-registry.apps.rm2.thpm.p1.openshiftapps.com"
+            OCP_PROJECT = "armada009-dev"
+            OCP_USER = "armada009"
             QUAY_REGISTRY = "quay.io/armada009"
             QUAY_REPO = "rental-car-web"
+            QUAY_USERNAME = "armada009"
+            QUAY_PASSWORD = ";4Qi68Rp"
         }
 
         parameters {
@@ -94,11 +99,61 @@ def call(Map config = [:]) {
             stage('Build & Push Docker Image') {
                 steps {
                     script {
-                        def imageTag = "${env.QUAY_REGISTRY}/${env.QUAY_REPO}:${params.TAG}"
+                        def appName = sh(script: 'node -p "require(\'./package.json\').name"', returnStdout: true).trim()
+                        def appFullVersion = sh(script: 'node -p "require(\'./package.json\').version"', returnStdout: true).trim()
+                            
+                        def imageTag = "${env.appName}:${appFullVersion}"
+                        def ocpImageTag = "${env.OCP_REGISTRY}/${env.OCP_PROJECT}/${imageTag}"
                         sh """
+                            set -x
+                            set -e
                             docker build -t ${imageTag} .
-                            docker login -u="armada009" -p=";4Qi68Rp" quay.io
-                            docker push ${imageTag}
+                            docker tag ${imageTag} ${ocpImageTag}
+                            docker login -p ${env.OCP_TOKEN} -u ${env.OCP_USER} ${env.OCP_REGISTRY}
+                            docker push ${ocpImageTag}
+                        """
+                    }
+                }
+            }
+
+            stage('Docker Image Archive') {
+                steps {
+                    script {
+                        def appName = sh(script: 'node -p "require(\'./package.json\').name"', returnStdout: true).trim()
+                        def appFullVersion = sh(script: 'node -p "require(\'./package.json\').version"', returnStdout: true).trim()
+                            
+                        def imageTag = "${env.appName}:${appFullVersion}"
+                        def ocpImageTag = "${env.OCP_REGISTRY}/${env.OCP_PROJECT}/${imageTag}"
+                        def quayImageTag = "${env.QUAY_REGISTRY}/${env.QUAY_REPO}/${imageTag}"
+                        sh """
+                            set -x
+                            set -e
+                            docker login -p ${env.OCP_TOKEN} -u ${env.OCP_USER} ${env.OCP_REGISTRY}
+                            docker push ${ocpImageTag}
+                            docker tag ${ocpImageTag} ${quayImageTag}
+                            docker login -u=${env.QUAY_USERNAME} -p=${env.QUAY_PASSWORD} quay.io
+                            docker push ${quayImageTag}
+                        """
+                    }
+                }
+            }
+
+            stage('OCP ConfigMap') {
+                steps {
+                    script {
+                        def appName = sh(script: 'node -p "require(\'./package.json\').name"', returnStdout: true).trim()
+                        def appFullVersion = sh(script: 'node -p "require(\'./package.json\').version"', returnStdout: true).trim()
+                        def gitCommitId = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+                        sh """
+                            set -x
+                            set -e
+                            export APP_CONFIG_DATA='key-value'
+                            oc login --token=${env.OCP_TOKEN} --server=${env.OCP_CLUSTER_URL}
+                            oc project ${env.OCP_PROJECT}
+                            oc process -f cicd/configmap.yaml -n ${env.OCP_PROJECT} \
+                            -p APP_NAME=${appName} -p APP_FULL_VERSION=${appFullVersion}\
+                            -p GIT_COMMIT_ID=${gitCommitId} -p JENKINS_BUILD_NUMBER=${env.BUILD_NUMBER}\
+                            -p CONFIG_DATA=${env.APP_CONFIG_DATA} | oc apply -n ${env.OCP_PROJECT} -f -
                         """
                     }
                 }
@@ -109,14 +164,16 @@ def call(Map config = [:]) {
                     script {
                         try {
                             sh """
+                                set -x
+                                set -e
                                 oc login --token=${env.OCP_TOKEN} --server=${env.OCP_CLUSTER_URL}
                                 oc project ${env.OCP_PROJECT}
-                                
-                                # Ensure deployment exists
-                                oc apply -f cicd/deployment.yaml
 
-                                # Now update the image
-                                oc set image deployment/react-vite-app react-vite-app=${env.QUAY_REGISTRY}/${env.QUAY_REPO}:${params.TAG}
+                                oc process -f cicd/deployment.yaml -n ${env.OCP_PROJECT} \
+                                -p APP_NAME=${appName} -p APP_FULL_VERSION=${appFullVersion}\
+                                -p GIT_COMMIT_ID=${gitCommitId} -p JENKINS_BUILD_NUMBER=${env.BUILD_NUMBER}\
+                                -p CONFIG_DATA=${env.APP_CONFIG_DATA} | oc apply -n ${env.OCP_PROJECT} --force=true -f -
+                                oc rollout restart ${env.appName}:${appFullVersion} -n ${env.OCP_PROJECT}
 
                                 echo "Deployment successful"
                             """
